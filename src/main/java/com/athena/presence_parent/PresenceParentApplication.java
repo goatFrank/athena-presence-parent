d9bd51062b5a2d00892cec7176a2961d3702a984
goatFrank <frankxspotify@gmail.com>
2026-02-20 20:28:26 +0100

    ++ commit iniziale

---
+++ src/main/java/com/athena/presence_parent/PresenceParentApplication.java
@@ -0,0 +1,13 @@
+package com.athena.presence_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athena/presence_parent/PresenceParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athena.presence_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
